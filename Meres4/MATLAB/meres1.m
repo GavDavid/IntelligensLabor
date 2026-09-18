8282af56f5376c30d527105de796a8861ebbb5f4
PaRIK22 <palpatrik04@gmail.com>
2026-09-18 18:18:41 +0200

    Jegyzőkönyv alpok, 1. feladat
    Jegyzőkönyv alpok, 1. feladat

--- Meres4/MATLAB/meres1.m
+++ Meres4/MATLAB/meres1.m
@@ -1,4 +1,6 @@
 clear;
+close all;
+clc;
 
 Sensi = 9.8; % [pC/g] / 0.998 [pC/ms-2]
 Gain = 0.1; % [Volt/Unit out]
@@ -6,14 +8,32 @@ MeasName = 'meres1_mono.wav';
 
 [MeasData, MeasRate] = audioread(MeasName);
 
-%%Spectogram
+%% Spektrogram
+windowLength = 15000;
+[~, Frequency, Time, Power] = spectrogram(...
+	MeasData, windowLength, [], windowLength, MeasRate);
+
 figure(1)
-spectrogram(MeasData, 15000, [], [], MeasRate, 'yaxis')
+imagesc(Time, Frequency / 1000, 10 * log10(Power));
+axis xy
+xlabel('Idő [s]')
+ylabel('Frekvencia [kHz]')
+title('A ventilátor rezgésjelének spektrogramja')
+colorbar
+clim([-150 -70])
+ylim([0 3])
+grid on
 
 
-%%Data converted to acceleration values
-AccData = MeasData / (0.1 * 0.998) * (0.04 / 0.0054);
+%% A mérési jel átszámítása gyorsulásértékre
+AccData = MeasData / (Gain * 0.998) * (0.04 / 0.0054);
+TimeAcc = (0:length(AccData) - 1) / MeasRate;
+
 figure(2)
-plot(AccData)
+plot(TimeAcc, AccData)
+xlabel('Idő [s]')
+ylabel('Gyorsulás [m/s^2]')
+title('A rezgésjel nagysága')
+grid on
 
 
